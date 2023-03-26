08b805be27f1a21fc3716fa88e6bb013dbb44476
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2023-03-26 20:12:28 +0300

    add contact me page objects

---
+++ tests/dovydas.xyz/objects/pages/ContactMePage.ts
@@ -0,0 +1,54 @@
+import { Locator, Page, expect } from '@playwright/test'
+import { AbstractPage } from './AbstractPage'
+
+
+export enum ContactMeLink {
+    blogFeed,
+    pixelfedProfile,
+    pixelfedFeed,
+    mastodonProfile,
+    mastodonFeed,
+    emailAndPhone,
+  }
+
+
+export class ContactMePage extends AbstractPage {
+  readonly blogFeed: Locator
+  readonly pixelfedProfile: Locator
+  readonly pixelfedFeed: Locator
+  readonly mastodonProfile: Locator
+  readonly mastodonFeed: Locator
+  readonly emailAndPhone: Locator
+
+
+  constructor(page: Page) {
+    super(page, 'ContactMePage', '/contact-me')
+    this.blogFeed = page.getByRole('listitem').filter({ hasText: 'Blog: feed' }).getByRole('link', { name: 'feed' })
+    this.pixelfedProfile = page.getByRole('listitem').filter({ hasText: 'Pixelfed: profile, feed' }).getByRole('link', { name: 'profile' })
+    this.pixelfedFeed = page.getByRole('listitem').filter({ hasText: 'Pixelfed: profile, feed' }).getByRole('link', { name: 'feed' })
+    this.mastodonProfile = page.getByRole('listitem').filter({ hasText: 'Mastodon: profile, feed' }).getByRole('link', { name: 'profile' })
+    this.mastodonFeed = page.getByRole('listitem').filter({ hasText: 'Mastodon: profile, feed' }).getByRole('link', { name: 'feed' })
+    this.emailAndPhone = page.getByRole('link', { name: 'in the resume' })
+  }
+
+
+  selectLocator(locatorConstant: ContactMeLink): Locator {
+    switch (locatorConstant){
+      case ContactMeLink.blogFeed:
+        return this.blogFeed
+      case ContactMeLink.pixelfedProfile:
+        return this.pixelfedProfile
+      case ContactMeLink.pixelfedFeed:
+        return this.pixelfedFeed
+      case ContactMeLink.mastodonProfile:
+        return this.mastodonProfile
+      case ContactMeLink.mastodonFeed:
+        return this.mastodonFeed
+      case ContactMeLink.emailAndPhone:
+        return this.emailAndPhone
+      default:
+        this.throwOnMissingCase(locatorConstant)
+    }
+  }
+
+}
--- tests/dovydas.xyz/tests/visual/visual-all-pages.spec.ts
+++ tests/dovydas.xyz/tests/visual/visual-all-pages.spec.ts
@@ -2,6 +2,7 @@ import {test, expect} from '@playwright/test'
 import { HomePage } from '../../objects/pages/HomePage'
 import { ResumePage } from '../../objects/pages/ResumePage'
 import { ProjectsPage } from '../../objects/pages/ProjectsPage'
+import { ContactMePage } from '../../objects/pages/ContactMePage'
 
 test.describe.parallel('Visual Regression Tests', () => {
 
@@ -19,6 +20,12 @@ test.describe.parallel('Visual Regression Tests', () => {
     })
     test('Projects snapshot.', async ({ page }) => {
         let testedPage: ProjectsPage = new ProjectsPage(page)
+        await testedPage.visit()
+        expect(await page.screenshot()).toMatchSnapshot(testedPage.getSnapshotName())
+        
+    })
+    test('Contact me snapshot.', async ({ page }) => {
+        let testedPage: ContactMePage = new ContactMePage(page)
         await testedPage.visit()
         expect(await page.screenshot()).toMatchSnapshot(testedPage.getSnapshotName())
         
