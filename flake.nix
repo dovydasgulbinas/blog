e75abed03ab103f8f760b395c1548748835797d3
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-02-05 17:12:10 +0200

    add basic flake environment

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "Dovydas.xyz blog runtime";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    oldnix.url = "github:nixos/nixpkgs?ref=nixos-21.11";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs, ... } @ inputs: 
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      oldpkgs = inputs.oldnix.legacyPackages.x86_64-linux;
+    in
+    {
+    packages.x86_64-linux.dovydas-xyz = pkgs.lektor;
+    packages.x86_64-linux.default = pkgs.lektor;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_23 
+          pkgs.lektor 
+          pkgs.pre-commit
+        ];
+        shellHook = ''
+          echo "Activating dovydas.xyz Dev Shell!"
+        '';
+      };
   };
 }
